4f8011367a82136558773e730effcc401e9cf314
Brandon Gormley <brandontgormley@gmail.com>
2024-05-06 14:55:20 -0500

    add footer

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,15 @@
+export default function Footer() {
+    return (
+        <footer className='w-full bg-white flex justify-center items-center text-sm text-gray-600'>
+            <p className='py-4'>
+                💻 Design/Dev:{' '}
+                <a
+                    className='mx-auto text-center font-semibold'
+                    href='https://www.github.com/BrandonGormley'
+                >
+                    BrandonGormley
+                </a>
+            </p>
+        </footer>
+    );
+}
