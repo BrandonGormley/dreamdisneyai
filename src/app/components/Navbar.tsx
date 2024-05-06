1b86220d263ca0698a2f75e88ebd609d42374c7d
Brandon Gormley <brandontgormley@gmail.com>
2024-05-06 12:10:46 -0500

    update navbar styling

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -6,15 +6,21 @@ export default function Navbar() {
             <Link href={'/'} className='flex flex-row items-center'>
                 <h1 className='text-lg tracking-tighter text-gray-800'>
                     DreamDisney{' '}
-                    <span className='text-sm text-lightblue'>AI</span>
+                    <span className='text-xs text-lightblue border-lightblue-dark'>
+                        AI
+                    </span>
                 </h1>
             </Link>
 
             <nav className='flex flex-row items-center'>
                 <ul className='flex items-center text-gray-600'>
-                    <li className='mx-2'>
-                        <Link className='hover:text-gray-800' href={'/parks'}>
-                            Item 1
+                    <li>
+                        <Link
+                            className='transition bg-gradient-to-r bg-white text-lightblue px-4 py-1 rounded-full hover:shadow-m lg:max-w-[200px] border border-lightblue hover:bg-lightblue hover:text-white'
+                            href={'/#'}
+                            aria-disabled
+                        >
+                            Login
                         </Link>
                     </li>
                 </ul>
