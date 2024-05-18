bf3b9e08555c46674d430f8f56322a60e4f6bf64
Brandon Gormley <brandontgormley@gmail.com>
2024-05-18 12:04:55 -0500

    add food selector component

---
+++ src/app/components/ui/planner/FoodSelector.tsx
@@ -0,0 +1,16 @@
+export default function FoodSelector() {
+    return (
+        <div className='flex flex-col items-center justify-center animate-fadeIn'>
+            <p className='max-w-xl text-lg my-2 text-gray-800 text-center'>
+                To ensure everyone enjoys the culinary magic of Disney World,
+                let's first explore the types of food you're craving. Are you in
+                the mood for classic American fare, international cuisines, or
+                delightful Disney-themed treats?
+            </p>
+
+            <p className='max-w-xl text-sm my-2 text-gray-600 text-center'>
+                Please select all that food types for your trip.
+            </p>
+        </div>
+    );
+}
