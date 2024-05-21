fce2d87e1a6ee0990e4e9b8be647307fea46878e
Brandon Gormley <brandontgormley@gmail.com>
2024-05-21 14:15:46 -0500

    add food selector icons

--- src/app/components/ui/planner/FoodSelector.tsx
+++ src/app/components/ui/planner/FoodSelector.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+
 export default function FoodSelector() {
     return (
         <div className='flex flex-col items-center justify-center animate-fadeIn'>
@@ -11,6 +13,63 @@ export default function FoodSelector() {
             <p className='max-w-xl text-sm my-2 text-gray-600 text-center'>
                 Please select all that food types for your trip.
             </p>
+            <div className='flex flex-row justify-center items-center flex-wrap w-full lg:max-w-[60%]'>
+                <div
+                    className={`flex flex-col justify-start items-center p-2 rounded-lg border border-gray-200 shadow-md m-2 w-32 hover:scale-105 transition-all cursor-pointer duration-300`}
+                >
+                    <Image
+                        src={'/italian-food.png'}
+                        alt='Italian Food Icon'
+                        width={50}
+                        height={50}
+                    />
+                    <p className='text-xs my-2'>Italian</p>
+                </div>
+                <div
+                    className={`flex flex-col justify-start items-center p-2 rounded-lg border border-gray-200 shadow-md m-2 w-32 hover:scale-105 transition-all cursor-pointer duration-300`}
+                >
+                    <Image
+                        src={'/asian-food.png'}
+                        alt='Asian Food Icon'
+                        width={50}
+                        height={50}
+                    />
+                    <p className='text-xs my-2'>Asian</p>
+                </div>
+                <div
+                    className={`flex flex-col justify-start items-center p-2 rounded-lg border border-gray-200 shadow-md m-2 w-32 hover:scale-105 transition-all cursor-pointer duration-300`}
+                >
+                    <Image
+                        src={'/barbeque-food.png'}
+                        alt='Barbeque Food Icon'
+                        width={50}
+                        height={50}
+                    />
+                    <p className='text-xs my-2'>BBQ</p>
+                </div>
+                <div
+                    className={`flex flex-col justify-start items-center p-2 rounded-lg border border-gray-200 shadow-md m-2 w-32 hover:scale-105 transition-all cursor-pointer duration-300`}
+                >
+                    <Image
+                        src={'/snacks-food.png'}
+                        alt='Snacks Icon'
+                        width={50}
+                        height={50}
+                    />
+                    <p className='text-xs my-2'>Snacks</p>
+                </div>
+                <div
+                    className={`flex flex-col justify-start items-center p-2 rounded-lg border border-gray-200 shadow-md m-2 w-32 hover:scale-105 transition-all cursor-pointer duration-300`}
+                >
+                    <Image
+                        src={'/sweets-food.png'}
+                        alt='Sweets Icon'
+                        width={50}
+                        height={50}
+                    />
+                    <p className='text-xs my-2'>Sweets</p>
+                </div>
+            </div>
         </div>
     );
 }
